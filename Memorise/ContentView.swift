d34b01502f9cf08aa154279528651fdac142f911
Leigh De La Fontaine <leigh.dlf@icloud.com>
2020-06-01 13:53:07 +1000

    Finished first lecture.

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,12 +8,48 @@
 
 import SwiftUI
 
+// ContentView 'behaves' like a view. The rectangle of this view is the entire screen.
+// var body is a property. some View type is any type that behaves like a view.
+// Think of views like legos. They can be put together. ZStack is a combiner view
+// return isn't needed in one liners.
+// Text also behaves like a view.
+// Having modifiers on ZStack scopes it; applies to all.
+// padding applies to entire ZStack, foregroundColor to its children.
+// index is iteration var. ForEach isn't layout like ZStack.
+// Padding and spacing(in stacks) is generally kept standard.
+// if nor args then () can be removed, if last arg is {} cat sit outside.
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
 
+
+// Factored out of encapsulation; out of HStack { ForEach }
+// isFaceUp has needs to be initialised when called.
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack { if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+// Connects to canvas preview.
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
