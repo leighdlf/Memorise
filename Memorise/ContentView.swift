de77705c24baa8f40086fa53a5b8d8746346e416
Leigh De La Fontaine <leigh.dlf@icloud.com>
2020-06-01 12:09:16 +1000

    Initial Commit

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by Leigh De La Fontaine on 1/6/20.
+//  Copyright © 2020 Leigh De La Fontaine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
